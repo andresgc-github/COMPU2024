cb83a605c44a3ebdad161d846881108a5041881f
andresgc <andresgc.correo@gmail.com>
2024-04-11 17:21:46 +0200

    QUINTO COMMIT

--- mainSistemaSolar.cpp
+++ mainSistemaSolar.cpp
@@ -25,12 +25,12 @@ int main(){
 
     // DECLARACIÓN DE VARIABLES
     // Este array almacena las 9 masas de los cuerpos del sistema solar, empezando por el Sol -- ANTIGUO
-    double m[SIZExy]={0.0}; // El [SIZExy][SIZEt] lo tengo puesto por sintaxis de las funciones que uso debajo
+    double m[SIZExy]={1.0}; // El [SIZExy][SIZEt] lo tengo puesto por sintaxis de las funciones que uso debajo
     // Estos arrays son matrices 18x2, y almacena en este orden: x1, y1, x2, y2, etc. Hace esto para el instante t y para el instante t+h
     // Guarda las componentes x en los índices pares, y las componentes y en los índices impares
-    double r[SIZExy]={0.0}; 
-    double v[SIZExy]={0.0};
-    double a[SIZExy]={0.0};
+    double r[SIZExy]={1.0}; 
+    double v[SIZExy]={1.0};
+    double a[SIZExy]={1.0};
     // A continuación declaro strings con los nombres archivos de texto que uso para leer y escribir los datos y resultados
     string masasini="masasini";
     string posicionesini="posicionesini";
@@ -64,47 +64,108 @@ int main(){
 // BLOQUE 2: evolución temporal
     // DECLARACIÓN DE VARIABLES
     // Declaro la variable auxiliar w[][] del algoritmo de Verlet
-    double w[SIZExy]={0.0};
+    double w[SIZExy]={1.0};
     // Declaro el índice de planetas
     int i;
     // Declaro los reescalamientos para usarlos en el paso temporal
-    double Ms=1.99E30; //Cambio de unidades: m'=m/Ms
-    double c=149.6E9; //Cambio de unidades de las posiciones: r'=r/c
-    double k=sqrt(6.67E-11*Ms/(c*c*c));
+    double Ms=1.99e30; //Cambio de unidades: m'=m/Ms
+    double c=149.6e9; //Cambio de unidades de las posiciones: r'=r/c
+    double k=sqrt(6.67e-11*Ms/(c*c*c));
     // Declaro el tiempo y el paso temporal (unidad 58,1 días)
     double t=0.0;
-    double h=0.1*k; // Paso temporal. Con h=0.003, Mercurio tarda 500 iteraciones en dar 1 vuelta
+    double h=80000.0; // Paso temporal. Con h=0.003, Mercurio tarda 500 iteraciones en dar 1 vuelta
 
     do{
         for(i=0;i<SIZExy;i++){
-          /*  //r[i][0]=r[i][1];
+            //r[i][0]=r[i][1];
             //v[i][0]=v[i][1];
             //a[i][0]=a[i][1];
             r[i]+=h*v[i]+0.5*h*h*a[i];
-            //w[i]=v[i]+0.5*h*a[i];
-            w[i]=a[i];  */
-            v[i]+=0.5*h*a[i];
-            r[i]+=h*v[i];
+        }
+        for(i=0;i<SIZExy;i++){
+            w[i]=v[i]+0.5*h*a[i];
+            //w[i]=a[i];  
+            //v[i]+=0.5*h*a[i];
+            //r[i]+=h*v[i];
         }
         Escribir(r,posicionesR);
         CalcularAceleracion(a,m,r);
         //Escribir(a,aceleracionesR);
         for(i=0;i<SIZExy;i++){
-            //v[i]=w[i]+0.5*h*a[i];
-         /* v[i]+=0.5*h*(w[i]+a[i]);  */ 
-            v[i]+=0.5*h*a[i];         
+            v[i]=w[i]+0.5*h*a[i];
+            //v[i]+=0.5*h*(w[i]+a[i]);   
+            //v[i]+=0.5*h*a[i];         
         }
         //Escribir(v,velocidadesR);
         t+=h;
         //cout << t << endl;
 
-    }while(t<1000000*k);
+    }while(t<4000000*h);
 
 
     return 0;
 }
 
 
+// Esta función reescala la "columna" 1 de cualquier array (x[][1]) segun el tipo de reescalado que se elija
+void Reescalar(double x[SIZExy],int tipo){
+
+    double Ms=1.99e30; //Cambio de unidades: m'=m/Ms
+    double c=149.6e9; //Cambio de unidades de las posiciones: r'=r/c
+    double k=sqrt((6.67e-11)*Ms/(c*c*c)); //Cambio de unidades temporales: t'=kt
+
+    int i;
+
+    if(tipo==1){ //Tipo 1= masas. Pasa de kg a masas solares
+        for(i=0;i<SIZExy;i++){
+            x[i]=x[i]/(Ms*1.0);
+        }
+    } else if(tipo==2){ //Tipo 2= posiciones. Pasa de m (¡no kilómetros!) a distancias Tierra-Sol
+        for(i=0;i<SIZExy;i++){
+            x[i]=x[i]/(c*1.0);
+        }
+    } else if(tipo==3){ //Tipo 3= velocidades. Pasa de m/s (¡no km/s!) a distancias Tierra-Sol entre tiempos reescalados
+        for(i=0;i<SIZExy;i++){
+            x[i]=x[i]*1.0/(c*k);
+        }
+    } else {return;} // ¿¿¿??? Me he inventado la sintaxis, pero funciona. Es redundante, though.
+    
+
+    return; // Es redundante pero lo meto por un comentario que hizo mi profesor de programación
+}
+
+// Esta función calcula la aceleración de todos los cuerpos en función de sus posiciones (Newton)
+void CalcularAceleracion(double a[SIZExy],double m[SIZExy],double r[SIZExy]){
+    
+    int i,j;
+    //double ax=0.0;
+    //double ay=0.0;
+    double dx=0.0;
+    double dy=0.0;
+    double modulo=0.0;
+    double radical=0.0;
+    //double G=6.67E-11;
+
+    
+    for(i=0;i<SIZExy;i+=2){ // Hay tantas iteraciones como cuerpos en i, y como cuerpos-1 en j
+        //ax=0.0; ay=0.0; // Tengo que reiniciar esto para la siguiente iteración
+        a[i]=0.0; a[i+1]=0.0;
+        for(j=0;j<SIZExy;j+=2){ // Uso += por limpieza
+            if(j!=i){
+                    dx=r[i]-r[j]; // Resta de componentes X para cuerpos j-ésimo e i-ésimo, xj-xi
+                    dy=r[i+1]-r[j+1]; // Resta de componentes Y para cuerpos j-ésimo e i-ésimo, yj-yi
+                    radical=dx*dx+dy*dy;
+                    modulo=sqrt(radical); // Módulo=|rj-ri|
+                    a[i]+=-(1.0*m[j]*dx)/(modulo*modulo*modulo); // ax=sum(j!=i)mj xj-xi/|rj-ri|^3
+                    a[i+1]+=-(1.0*m[j]*dy)/(modulo*modulo*modulo); // ay=sum(j!=i)mj yj-yi/|rj-ri|^3
+            } else continue;
+        }
+        //a[i]=ax; // La fórmula es a=G*sum(j!=i) rj-ri/|rj-ri|^3. Ya tengo la suma, falta multiplicar
+        //a[i+1]=ay; // Así multiplico por G una sola vez y no en cada iteración
+    }
+    
+    return;
+}
 
 void Leer(double x[SIZExy],string filename,int tipo){
 
@@ -127,63 +188,6 @@ void Leer(double x[SIZExy],string filename,int tipo){
     fclose(f1);
 
 
-    return;
-}
-
-// Esta función reescala la "columna" 1 de cualquier array (x[][1]) segun el tipo de reescalado que se elija
-void Reescalar(double x[SIZExy],int tipo){
-
-    double Ms=1.99E30; //Cambio de unidades: m'=m/Ms
-    double c=149.6E9; //Cambio de unidades de las posiciones: r'=r/c
-    double k=sqrt(6.67E-11*Ms/(c*c*c)); //Cambio de unidades temporales: t'=kt
-
-    int i;
-
-    if(tipo==1){ //Tipo 1= masas. Pasa de kg a masas solares
-        for(i=0;i<SIZExy;i++){
-            x[i]=x[i]/Ms;
-        }
-    } else if(tipo==2){ //Tipo 2= posiciones. Pasa de m (¡no kilómetros!) a distancias Tierra-Sol
-        for(i=0;i<SIZExy;i++){
-            x[i]=x[i]/c;
-        }
-    } else if(tipo==3){ //Tipo 3= velocidades. Pasa de m/s (¡no km/s!) a distancias Tierra-Sol entre tiempos reescalados
-        for(i=0;i<SIZExy;i++){
-            x[i]=x[i]*1.0/(c*k);
-        }
-    } else {return;} // ¿¿¿??? Me he inventado la sintaxis, pero funciona. Es redundante, though.
-    
-
-    return; // Es redundante pero lo meto por un comentario que hizo mi profesor de programación
-}
-
-// Esta función calcula la aceleración de todos los cuerpos en función de sus posiciones (Newton)
-void CalcularAceleracion(double a[SIZExy],double m[SIZExy],double r[SIZExy]){
-    
-    int i,j;
-    double ax=0.0;
-    double ay=0.0;
-    double dx=0.0;
-    double dy=0.0;
-    double modulo=0.0;
-    double G=6.67E-11;
-
-    
-    for(i=0;i<SIZExy;i+=2){ // Hay tantas iteraciones como cuerpos en i, y como cuerpos-1 en j
-        ax=0.0; ay=0.0; // Tengo que reiniciar esto para la siguiente iteración
-        for(j=0;j<SIZExy;j+=2){ // Uso += por limpieza
-            if(j!=i){
-                    dx=r[j]-r[i]; // Resta de componentes X para cuerpos j-ésimo e i-ésimo, xj-xi
-                    dy=r[j+1]-r[i+1]; // Resta de componentes Y para cuerpos j-ésimo e i-ésimo, yj-yi
-                    modulo=sqrt(dx*dx+dy*dy); // Módulo=|rj-ri|
-                    ax+=(1.0*m[i]*dx)/(modulo*modulo*modulo); // ax=sum(j!=i)mj xj-xi/|rj-ri|^3
-                    ay+=(1.0*m[i]*dy)/(modulo*modulo*modulo); // ay=sum(j!=i)mj yj-yi/|rj-ri|^3
-            } else continue;
-        }
-        a[i]=ax; // La fórmula es a=G*sum(j!=i) rj-ri/|rj-ri|^3. Ya tengo la suma, falta multiplicar
-        a[i+1]=ay; // Así multiplico por G una sola vez y no en cada iteración
-    }
-    
     return;
 }
 
